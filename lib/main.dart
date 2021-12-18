c33ba384a0af2429c1468d4c90c8e9f435f3c210
Maanas Nair <bee.birdiecorporation@gmail.com>
2021-12-18 20:18:30 +0530

    backend: temporarily remove null safety

--- lib/main.dart
+++ lib/main.dart
@@ -110,7 +110,6 @@ class _MyHomePageState extends State<MyHomePage> {
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: const Icon(Icons.add),
-        connect();
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
